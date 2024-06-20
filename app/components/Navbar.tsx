67e844031e56114e5e393b619cdb0a371b1e31b9
Harsh Shah <71080727+devHarshShah@users.noreply.github.com>
2024-06-21 00:06:11 +0530

    Chore: Fixed typo error

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 right-0 bg-[#FAE9DA] p-2 flex items-center justify-between z-20 font-poppins">
       <div>
-        <Image src="/logo.png" alt="Logo" width={180} height={180} className="ml-[4rem]" />
+        <Image src="/Logo.png" alt="Logo" width={180} height={180} className="ml-[4rem]" />
       </div>
       <div className="flex justify-center items-center space-x-12 font-semibold mr-[2rem]">
         <Link href="#" className="text-zinc-800">
