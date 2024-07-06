40197b8a2b14ded69335697dff4c6abdea3e1993
Harsh Shah <71080727+devHarshShah@users.noreply.github.com>
2024-07-06 23:18:07 +0530

    chore: fixing resolution error

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image';
 
 const Services = () => {
   return (
-    <div className="flex flex-col md:flex-row justify-center items-center px-5 md:px-[3rem] m-5 md:m-10 mb-9" id='our-services'>
-      <div className="w-full md:w-[30%] flex flex-col justify-center items-start space-y-4">
+    <div className="flex flex-col md:flex-row justify-center items-center px-5 md:px-[3rem] m-5 md:m-10 mb-9 min-h-[90vh]" id='our-services'>
+      <div className="w-full md:w-[35%] flex flex-col justify-center items-start space-y-4">
         <h1 className="text-5xl w-[50%] md:text-8xl text-[#3C130E] font-nohemi">Our Services</h1>
         <p className="text-md md:text-xl p-2 font-neue">Easily connect with certified, loving dog walkers through trusted platforms, ensuring your pets well-being and your peace of mind.</p>
         <button className="bg-[#FC6638] rounded-[2rem] py-2 px-4 md:px-7 text-lg md:text-xl text-white font-semibold">Book Now</button>
       </div>
-      <div className="w-full md:w-[70%] flex flex-col md:flex-row justify-center items-center md:pl-20">
+      <div className="w-full md:w-[65%] flex flex-col md:flex-row justify-center items-center md:pl-20">
         <div className="w-full md:w-1/2 flex flex-col justify-center items-start m-5 md:m-10 space-y-4">
           <div className="bg-[#FEEDDD] h-[25rem] w-[22rem] p-8 flex justify-center items-center rounded-tl-[5rem] rounded-br-[5rem] shadow-xl">
             <video width="300" height="300" autoPlay loop muted>
