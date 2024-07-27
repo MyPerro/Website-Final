9b8d86f173503ddea8a987b699bb92e7b9dc13c3
Harsh Shah <71080727+devHarshShah@users.noreply.github.com>
2024-07-27 17:55:37 +0530

    fix: final bugs

--- app/components/cardBlock.tsx
+++ app/components/cardBlock.tsx
@@ -48,16 +48,15 @@ const settings = {
 
   return (
 <>
-<h1 className='font-nohemi text-6xl  mt-12 ml-10' id='blog-card'>Blogs </h1>
+<h1 className='font-nohemi text-6xl mt-12 ml-10' id='blog-card'>Blogs </h1>
 <Slider className="p-[3rem] mb-10" {...settings}>
   {cardData.map((card, index) => (
   <div key={index} className={`shadow-md rounded-lg overflow-hidden bg-[#FEEAD8] blog-card ${index === activeSlide ? 'card-focused' : 'card-blurred'}`}>
     <div style={{ display: 'flex', width: '100%' }}>
       <Image src={card.image} width={150} height={150} alt={card.title} className="w-1/2 blog-slider-main" />
-      <div className="p-5 text-center flex flex-col justify-between w-1/2">
+      <div className="p-2 md:p-5 text-center flex flex-col justify-start space-y-6 md:space-y-0 md:justify-between w-1/2">
         <h2 className="text-sm xl:text-lg font-bold text-gray-800 pt-2 blog-desc-main">{card.title}</h2>
-        <p className="text-sm md:text-lg text-gray-600">{card.description}</p>
-        <button className="bg-orange-500 text-white font-bold md:mb-5 md:py-1 md:px-4 pt-1 px-2 mt-3 md:mt-6 rounded hover:bg-orange-600 transition duration-300 ease-in-out text-xs xl:text-lg" ><a href={card.link} target='_blank'>Read More</a></button>
+        <button className="bg-orange-500 text-white font-bold md:mb-5 md:py-1 md:px-4 pt-1 px-2 mt-1 md:mt-6 rounded hover:bg-orange-600 transition duration-300 ease-in-out text-xs xl:text-lg" ><a href={card.link} target='_blank'>Read More</a></button>
       </div>
     </div>
   </div>
