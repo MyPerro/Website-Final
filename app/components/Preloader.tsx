92da6b89ab6556693b0ed52b7e184d256e96334c
var-code-5 <varshithisgod@gmail.com>
2024-07-28 22:16:22 +0530

    fix:working blog buttons

--- app/components/Preloader.tsx
+++ app/components/Preloader.tsx
@@ -2,7 +2,7 @@ import { DotLottiePlayer, Controls, PlayerEvents } from '@dotlottie/react-player
 import '@dotlottie/react-player/dist/index.css';
 export default function Loader(){
     return(
-        <div className='flex flex-col justify-center items-center h-[100vh]' id='preloaded'>
+        <div className='preloader-css' id='preloaded'>
             <div className='h-[10vh]'>
             <DotLottiePlayer src="preloader.json" loop autoplay />
             </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
-'use client';
-import Image from 'next/image';
-import Services from './components/Services';
-import HeroSection from './components/HeroSection';
-import Ourteam from './components/OurTeam';
-import HoverParagraph from './components/Hoverpara';
-import Cardblock from './components/cardBlock';
-import Footer from './components/Footer';
-import Loader from './components/Preloader';
-import { useState , useEffect} from 'react';
-import Metadata from './components/MetaData';
-import dynamic from 'next/dynamic';
+"use client";
+import Image from "next/image";
+import Services from "./components/Services";
+import HeroSection from "./components/HeroSection";
+import Ourteam from "./components/OurTeam";
+import HoverParagraph from "./components/Hoverpara";
+import Cardblock from "./components/cardBlock";
+import Footer from "./components/Footer";
+import Loader from "./components/Preloader";
+import React, { useState, useEffect } from "react";
+import Metadata from "./components/MetaData";
+import dynamic from "next/dynamic";
 
-const SmartCollar = dynamic(() => import('./components/SmartCollar'), { ssr: false });
-
-
+const SmartCollar = dynamic(() => import("./components/SmartCollar"), {
+  ssr: false,
+});
 
 // export const metadata: Metadata = {
 //   title: 'MyPerro',
@@ -21,33 +21,31 @@ const SmartCollar = dynamic(() => import('./components/SmartCollar'), { ssr: fal
 // };
 
 export default function Home() {
-  const [isloaded,setLoaded] = useState(false);
-  useEffect(() =>{
-    setTimeout(() =>{const ele = document.getElementById("preloaded");
-      ele?.classList.add("pre-fade");},4000)
+  const [isloaded, setLoaded] = useState(false);
+  useEffect(() => {
     setTimeout(() => {
-      setLoaded(true);}, 4500);
-   })
-  if(isloaded){
+      const ele = document.getElementById("preloaded");
+      ele?.classList.add("pre-fade");
+    }, 4000);
+    setTimeout(() => {
+      const ele = document.getElementById("preloaded");
+      ele?.remove();
+    }, 4500);
+  });
   return (
     <>
-      <Metadata seoTitle='MyPerro' seoDescription="At MyPerro, we are here to revolutionize the dog care industry in India, including innovative dog boarding solutions. Our platform is powered by tech, allowing us to provide reliable, personalized, and secure dog care experiences. We understand the unique challenges faced by dog owners and are committed to offering seamless services, whether it's daily care or dog boarding. MyPerro isn't just a platform; it's a tech-driven promise to prioritize the well-being of your furry friends. Join us in reshaping dog care and boarding through the power of technology."/>
-      <HeroSection/>
-      <Services/>
-      <SmartCollar/>
-      <HoverParagraph/>
-      <Ourteam/>
-      <Cardblock/>
-      <Footer/>
+      <Metadata
+        seoTitle="MyPerro"
+        seoDescription="At MyPerro, we are here to revolutionize the dog care industry in India, including innovative dog boarding solutions. Our platform is powered by tech, allowing us to provide reliable, personalized, and secure dog care experiences. We understand the unique challenges faced by dog owners and are committed to offering seamless services, whether it's daily care or dog boarding. MyPerro isn't just a platform; it's a tech-driven promise to prioritize the well-being of your furry friends. Join us in reshaping dog care and boarding through the power of technology."
+      />
+      <HeroSection />
+      <Loader />
+      <Services />
+      <SmartCollar />
+      <HoverParagraph />
+      <Ourteam />
+      <Cardblock />
+      <Footer />
     </>
   );
-}
-else{
-  return(
-    <>
-      <Metadata seoTitle='MyPerro' seoDescription="At MyPerro, we are here to revolutionize the dog care industry in India, including innovative dog boarding solutions. Our platform is powered by tech, allowing us to provide reliable, personalized, and secure dog care experiences. We understand the unique challenges faced by dog owners and are committed to offering seamless services, whether it's daily care or dog boarding. MyPerro isn't just a platform; it's a tech-driven promise to prioritize the well-being of your furry friends. Join us in reshaping dog care and boarding through the power of technology."/>
-      <Loader /> 
-    </>
-  )
-}
 }
