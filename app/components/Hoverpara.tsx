719f422045e2cdbe5d7b0d0c7dbdfbb9cc95f9a0
var-code-5 <varshithisgod@gmail.com>
2024-07-29 19:06:06 +0530

    fix: hamburger menu and mission

--- app/components/Hoverpara.tsx
+++ app/components/Hoverpara.tsx
@@ -1,5 +1,5 @@
-'use client';
-import React, { useState, useEffect } from 'react';
+"use client";
+import React, { useState, useEffect } from "react";
 
 const HoverParagraph = () => {
   const [hovered, setHovered] = useState(false);
@@ -13,38 +13,78 @@ const HoverParagraph = () => {
     };
 
     // Check if window is defined to avoid SSR issues
-    if (typeof window !== 'undefined') {
+    if (typeof window !== "undefined") {
       // Set initial mobile state based on window width
       setIsMobile(window.innerWidth <= 1200);
 
       // Add event listener for resize
-      window.addEventListener('resize', handleResize);
+      window.addEventListener("resize", handleResize);
 
       return () => {
         // Cleanup: remove event listener
-        window.removeEventListener('resize', handleResize);
+        window.removeEventListener("resize", handleResize);
       };
     }
   }, []);
 
-  const toggleVisibility = (setter: React.Dispatch<React.SetStateAction<boolean>>) => {
+  const toggleVisibility = (
+    setter: React.Dispatch<React.SetStateAction<boolean>>
+  ) => {
     setter((prev: boolean) => !prev);
   };
 
   return (
     <div className="hoverpage bg-[#3C130E] xl:bg-transparent md:border-t-2 md:border-b-2 border-[#3C130E]">
-      <div className={`hover-container ${hovered ? 'visible' : ''}`} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
-        <span className={`hover-word ${isMobile ? 'hovered' : hovered ? 'hovered' : ''}`}>MISSION</span>
-          <p className={`hover-paragraph ${hovered ? 'visible' : ''} md:text-2xl lg:text-lg`}>
-            To inculcate technology in he pet care industry to provide personalized care, foster transparency, and security. We aim to empower pet parents to cater to work commitments or plan vacations stress free.By also helping pet sitters and dog walkers get paid according to their efforts thus
-            forming a chain of trust, care , love and contentment on both sides.
-          </p>
+      <div
+        className={`hover-container ${hovered ? "visible" : ""}`}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+      >
+        <span
+          className={`hover-word ${
+            isMobile ? "hovered" : hovered ? "hovered" : ""
+          }`}
+        >
+          MISSION
+        </span>
+        <p
+          className={`hover-paragraph ${
+            hovered ? "visible" : ""
+          } md:text-2xl lg:text-lg`}
+        >
+          Our mission is to integrate technology into the pet care industry,
+          providing personalized care, fostering transparency, and ensuring
+          security. We aim to empower pet parents to balance work commitments
+          and plan vacations with peace of mind, knowing their pets are in good
+          hands. Additionally, we seek to support pet sitters and dog walkers by
+          facilitating fair compensation for their efforts, thereby creating a
+          chain of trust, care, love, and contentment that benefits all
+          parties involved.
+        </p>
       </div>
-      <div className={`hover-container2 ${hovered2 ? 'visible2' : ''}`} onMouseEnter={() => setHovered2(true)} onMouseLeave={() => setHovered2(false)}>
-      <span className={`hover-word ${isMobile ? 'hovered' : hovered2 ? 'hovered' : ''}`}>VISION</span>
-        <p className={`hover-paragraph2 ${hovered2 ? 'visible2' : ''} md:text-2xl lg:text-lg`}>
-          To broaden our services to ensure your dog gets the utmost personalized care that it deserves. We aim to have our own boarding area- “MyPerro Space” and also expand into pet grooming, pet-friendly commute and pet food industry. We want to provide the best personalized care by understanding
-          the unique needs of every dog coming to us.
+      <div
+        className={`hover-container2 ${hovered2 ? "visible2" : ""}`}
+        onMouseEnter={() => setHovered2(true)}
+        onMouseLeave={() => setHovered2(false)}
+      >
+        <span
+          className={`hover-word ${
+            isMobile ? "hovered" : hovered2 ? "hovered" : ""
+          }`}
+        >
+          VISION
+        </span>
+        <p
+          className={`hover-paragraph2 ${
+            hovered2 ? "visible2" : ""
+          } md:text-2xl lg:text-lg`}
+        >
+          To broaden our services to ensure your dog gets the utmost
+          personalized care that it deserves. We aim to have our own boarding
+          area- “MyPerro Space” and also expand into pet grooming, pet-friendly
+          commute and pet food industry. We want to provide the best
+          personalized care by understanding the unique needs of every dog
+          coming to us.
         </p>
       </div>
     </div>
