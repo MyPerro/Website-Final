db8b205dd3fc3255de9f1047d1cf808d81629b3d
var-code-5 <varshithisgod@gmail.com>
2024-12-15 13:30:11 +0530

    Revert "Merge pull request #31 from var-code-5/main"
    This reverts commit b9127afa057b585629ff0d3df7272fc89091930b, reversing
    changes made to 9c62e427ff5e077a7b1c08bf09b1a6878cd7c3e2.

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Ourteam from "./components/OurTeam";
 import HoverParagraph from "./components/Hoverpara";
 import Cardblock from "./components/cardBlock";
 import Footer from "./components/Footer";
+import Loader from "./components/Preloader";
 import React, { useState, useEffect } from "react";
 import Metadata from "./components/MetaData";
 import dynamic from "next/dynamic";
@@ -20,6 +21,17 @@ const SmartCollar = dynamic(() => import("./components/SmartCollar"), {
 // };
 
 export default function Home() {
+  const [isloaded, setLoaded] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      const ele = document.getElementById("preloaded");
+      ele?.classList.add("pre-fade");
+    }, 4000);
+    setTimeout(() => {
+      const ele = document.getElementById("preloaded");
+      ele?.remove();
+    }, 4500);
+  });
   return (
     <>
       <Metadata
@@ -27,6 +39,7 @@ export default function Home() {
         seoDescription="At MyPerro, we are here to revolutionize the dog care industry in India, including innovative dog boarding solutions. Our platform is powered by tech, allowing us to provide reliable, personalized, and secure dog care experiences. We understand the unique challenges faced by dog owners and are committed to offering seamless services, whether it's daily care or dog boarding. MyPerro isn't just a platform; it's a tech-driven promise to prioritize the well-being of your furry friends. Join us in reshaping dog care and boarding through the power of technology."
       />
       <HeroSection />
+      <Loader />
       <Services />
       <SmartCollar />
       <HoverParagraph />
